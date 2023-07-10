f7abc5644de2e354d4160e222b00a246e8b6997a
RenatoNino <jorgenino07@gmail.com>
2023-07-09 22:48:03 -0500

    feat(conecction): change to postgress

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Provider } from './provider/entities/provider.entity';
 import { ProviderModule } from './provider/provider.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { getEnvPath } from './common/helper/env.helper';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+
+const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [
@@ -14,21 +17,23 @@ import { join } from 'path';
       rootPath: join(__dirname, '..', 'public'),
     }),
     ConfigModule.forRoot({
+      envFilePath,
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'renato123',
-        database: 'tdw_providers',
-        entities: [Provider],
-        synchronize: false, //true para que cree las tablas
-        autoLoadEntities: true,
-      }),
-    }),
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: () => ({
+    //     type: 'mysql',
+    //     host: 'localhost',
+    //     port: 3306,
+    //     username: 'root',
+    //     password: 'renato123',
+    //     database: 'tdw_providers',
+    //     entities: [Provider],
+    //     synchronize: false, //true para que cree las tablas
+    //     autoLoadEntities: true,
+    //   }),
+    // }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     ProviderModule,
   ],
   controllers: [AppController],
